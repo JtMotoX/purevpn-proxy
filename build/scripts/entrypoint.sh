a401a613dca93b66e93b64bce5f042e52e4370d8
JtMotoX <7191259+JtMotoX@users.noreply.github.com>
2022-06-03 00:33:14 -0700

    adding cmd

--- build/scripts/entrypoint.sh
+++ build/scripts/entrypoint.sh
@@ -23,46 +23,52 @@ bool() {
 
 . /etc/profile
 
-# CHECK TRANSMISSION DOWNLOADS CONNECTIVITY
-if ! /scripts/check_downloads_connection.sh; then exit 1; fi
+if [ "$1" = "run" ]; then
+	# CHECK TRANSMISSION DOWNLOADS CONNECTIVITY
+	if ! /scripts/check_downloads_connection.sh; then exit 1; fi
 
-# CHECK INTERNET CONNECTIVITY
-if ! /scripts/check_internet_connection.sh; then exit 1; fi
+	# CHECK INTERNET CONNECTIVITY
+	if ! /scripts/check_internet_connection.sh; then exit 1; fi
 
-# VPN
-echo "Starting vpn service . . ."
-sudo --preserve-env /scripts/start_vpn.sh
-logTailArr+=("/var/log/purevpn.log")
+	# VPN
+	echo "Starting vpn service . . ."
+	sudo --preserve-env /scripts/start_vpn.sh
+	logTailArr+=("/var/log/purevpn.log")
 
-# KILLSWITCH
-if "$killswitch" != "false"; then
-	echo 'y' | sudo --preserve-env /scripts/killswitch_enable.sh
-else
-	echo "Killswitch is NOT enabled"
+	# KILLSWITCH
+	if "$killswitch" != "false"; then
+		echo 'y' | sudo --preserve-env /scripts/killswitch_enable.sh
+	else
+		echo "Killswitch is NOT enabled"
+	fi
+
+	# PROXY
+	if [[ "${proxy_service}" == "3proxy" ]]; then
+		echo "Starting 3proxy proxy service . . ."
+		sudo --preserve-env /scripts/start_3proxy.sh
+		logTailArr+=("${proxy_dir}/logs/*.log")
+	elif [[ "${proxy_service}" == "dante" ]]; then
+		echo "Starting dante proxy service . . ."
+		sudo --preserve-env /scripts/start_dante.sh
+		logTailArr+=("/var/log/sockd.log")
+	else
+		echo "Not starting proxy service since none was defined"
+	fi
+
+	# TRANSMISSION
+	echo "Starting transmission . . ."
+	sudo --preserve-env /scripts/start_transmission.sh
+	logTailArr+=("/var/log/transmission.log")
+
+	echo "Container is up and running"
+
+	# STREAM LOG FILES TO CONTAINER OUTPUT
+	tail -n+1 -f ${logTailArr[@]} &
+
+	# START MONITORING CONNECTIVITY AND PROCESSES
+	/scripts/monitor.sh
+
+	exit
 fi
 
-# PROXY
-if [[ "${proxy_service}" == "3proxy" ]]; then
-	echo "Starting 3proxy proxy service . . ."
-	sudo --preserve-env /scripts/start_3proxy.sh
-	logTailArr+=("${proxy_dir}/logs/*.log")
-elif [[ "${proxy_service}" == "dante" ]]; then
-	echo "Starting dante proxy service . . ."
-	sudo --preserve-env /scripts/start_dante.sh
-	logTailArr+=("/var/log/sockd.log")
-else
-	echo "Not starting proxy service since none was defined"
-fi
-
-# TRANSMISSION
-echo "Starting transmission . . ."
-sudo --preserve-env /scripts/start_transmission.sh
-logTailArr+=("/var/log/transmission.log")
-
-echo "Container is up and running"
-
-# STREAM LOG FILES TO CONTAINER OUTPUT
-tail -n+1 -f ${logTailArr[@]} &
-
-# START MONITORING CONNECTIVITY AND PROCESSES
-/scripts/monitor.sh
+exec "$@"
